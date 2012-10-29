9ddea4d37f36d87fb623a1b51833db2d5a05d943
chris.mendoza <chris.mendoza@cmendoza.net>
2012-10-29 17:51:18 -0500

    Create initial package layout for PyORM

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='PyORM',
+    version='0.0.1',
+    author='Chris Mendoza',
+    author_email='chris.mendoza@pyorm.com',
+    packages=['pyorm', 'pyorm.test'],
+    url='http://github.com/chrismendoza/PyORM/',
+    license='LICENSE.txt',
+    description='an ORM',
+    long_description=readme,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    test_suite='runtests.collect'
+)
