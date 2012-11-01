90e1bbb288d8f54fcec2fb50fa03a63bf54abe8d
chris.mendoza <chris.mendoza@cmendoza.net>
2012-11-01 06:49:05 -0500

    Fix a couple errors with the unittests for calc_tokens

--- pyorm/test/expression_test.py
+++ pyorm/test/expression_test.py
@@ -11,7 +11,7 @@ class MockOwner(object):
 
 class CalcTokensTestCase(unittest.TestCase):
     def test_calc_tokens_new_expr(self):
-        expr1 = Expression(1, OP_ADD)
+        expr1 = Expression(1, 2, op=OP_ADD)
         expr2 = 1
 
         # calc_tokens should return a new expression containing
@@ -20,9 +20,10 @@ class CalcTokensTestCase(unittest.TestCase):
         self.assertEqual(new_expr._tokens[0].type, T_EXP)
         self.assertEqual(new_expr._tokens[1], Token(T_OPR, OP_SUB))
         self.assertEqual(new_expr._tokens[2].type, T_LIT)
+        self.assertNotEqual(id(new_expr), id(expr1))
 
     def test_calc_tokens_new_expr_right(self):
-        expr1 = Expression(1, OP_ADD)
+        expr1 = Expression(1, 2, op=OP_ADD)
         expr2 = 1
 
         # Test the same thing as above, but from the right side
@@ -30,27 +31,28 @@ class CalcTokensTestCase(unittest.TestCase):
         self.assertEqual(new_expr._tokens[2].type, T_EXP)
         self.assertEqual(new_expr._tokens[1], Token(T_OPR, OP_SUB))
         self.assertEqual(new_expr._tokens[0].type, T_LIT)
+        self.assertNotEqual(id(new_expr), id(expr1))
 
     def test_calc_tokens_append_tokens(self):
         # verify that calc_tokens returns the primary expression
         # with the secondary expression's tokens appended when the
         # operator matches
-        expr1 = Expression(1, OP_ADD)
+        expr1 = Expression(1, 2, op=OP_ADD)
         expr2 = 2
 
         new_expr = calc_tokens(expr1, expr2, op=OP_ADD)
-        self.assertEqual(id(new_expr), expr1)
+        self.assertEqual(id(new_expr), id(expr1))
 
     def test_calc_tokens_insert_tokens_right(self):
         # verify that calc_tokens returns the primary expression
         # with the secondary expression's tokens inserted at index
         # zero when the operator matches, and the primary 
         # expression is on the right.
-        expr1 = Expression(1, OP_ADD)
+        expr1 = Expression(1, 2, op=OP_ADD)
         expr2 = 2
 
         new_expr = calc_tokens(expr2, expr1, op=OP_ADD, right=True)
-        self.assertEqual(id(new_expr), expr1)
+        self.assertEqual(id(new_expr), id(expr1))
 
 
 class ExpressionTestCase(unittest.TestCase):
