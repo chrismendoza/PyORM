a05a20d35045cc1c3c8686e383d0fe4211b21688
chris.mendoza <chris.mendoza@cmendoza.net>
2013-02-14 23:31:38 -0600

    Package setup file for legacy pyorm

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
+readme = f.read()
+f.close()
+
+setup(
+    name='PyORM',
+    version='0.1.0',
+    author='Chris Mendoza',
+    author_email='chris.mendoza@pyorm.com',
+    packages=['pyorm'],
+    url='http://github.com/chrismendoza/PyORM/tree/legacy',
+    license='LICENSE.txt',
+    description='an ORM',
+    long_description=readme,
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
